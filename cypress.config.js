39995d57480eed1e5037fa8c042ac2e0e5dd976f
ef <2425066+drinkfalconpunch@users.noreply.github.com>
2022-07-27 07:47:19 -0400

    check footer. 1920x1080 resolution set

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,12 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: 'q4bw4p',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
   },
+  viewportWidth: 1920,
+  viewportHeight: 1080
 });
--- cypress/e2e/paperless.cy.js
+++ cypress/e2e/paperless.cy.js
@@ -1,3 +1,5 @@
+import "cypress-real-events/support";
+
 describe('test front page loads', () => {
   it('loads', () => {
     cy.visit('https://www.paperlessparts.com')
@@ -12,3 +14,22 @@ describe('home page has free demo link', () => {
   })
 })
 
+describe('key integrations hover link is valid', () => {
+  it('key integrations hover link is valid', () => {
+    cy.visit('https://www.paperlessparts.com')
+      .get('a') // get all links
+      .contains('Products') // find the ones with the text 'Products'
+      .realHover('mouse')
+      // .get('menu-item')
+      // .trigger('mouseover')
+      // .click()
+  })
+})
+
+describe('check footer for address', () => {
+  it('check footer for address', () => {
+    cy.visit('https://www.paperlessparts.com')
+      // .get('footer')
+      .contains('footer', '205 Portland Street, Level 6')
+  })
+})
