25ba1ec7188836370481230d19c43eda71c5e3a0
ef <2425066+drinkfalconpunch@users.noreply.github.com>
2022-07-27 09:28:25 -0400

    hover testing works

--- cypress/e2e/paperless.cy.js
+++ cypress/e2e/paperless.cy.js
@@ -1,35 +1,38 @@
-import "cypress-real-events/support";
+import "cypress-real-events/support"; // support for mouse hover
+
+const url = 'https://www.paperlessparts.com'
 
 describe('test front page loads', () => {
   it('loads', () => {
-    cy.visit('https://www.paperlessparts.com')
+    cy.visit(url)
   })
 })
 
 describe('home page has free demo link', () => {
   it('free demo link on front page', () => {
-    cy.visit('https://www.paperlessparts.com')
+    // finds the first link with the text "Book Free Demo"
+    cy.visit(url)
       .get('a')
       .contains('Book Free Demo')
   })
 })
 
 describe('key integrations hover link is valid', () => {
-  it('key integrations hover link is valid', () => {
-    cy.visit('https://www.paperlessparts.com')
-      .get('a') // get all links
-      .contains('Products') // find the ones with the text 'Products'
+  it.only('key integrations hover link is valid', () => {
+    cy.visit(url)
+      .get('#menu-header') // XPath: "//*[@id="menu-header"]/li[1]/a"
       .realHover('mouse')
-      // .get('menu-item')
-      // .trigger('mouseover')
-      // .click()
+      .find('a')
+      .contains('Key Integrations')
+      .click({ force: true })
   })
 })
 
 describe('check footer for address', () => {
   it('check footer for address', () => {
-    cy.visit('https://www.paperlessparts.com')
-      // .get('footer')
-      .contains('footer', '205 Portland Street, Level 6')
+    cy.visit(url)
+      .get('footer')
+      .should('contain', '205 Portland Street, Level 6')
+      // .contains('footer', '205 Portland Street, Level 6')
   })
 })
